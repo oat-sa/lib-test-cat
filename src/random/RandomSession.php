c1a8d4fabfb0d602f3df13d3d33c0d3393bf61a6
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-06-02 17:58:04 +0200

    testing cat implementation

---
+++ src/random/RandomSession.php
@@ -0,0 +1,57 @@
+<?php
+/**  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
+ * Copyright (c) 2017 (original work) Open Assessment Technologies SA
+ */
+
+namespace oat\libCat\random;
+
+use oat\libCat\CatSession;
+use oat\libCat\result\ResultVariable;
+
+class RandomSession implements CatSession
+{
+    
+    private $data; 
+
+    public function __construct($data){
+        $this->data = $data;
+    }
+
+    public function getTestMap($results = [])
+    {
+        $output = array_rand($this->data, rand(1, count($this->data)));
+        if(!is_array($output)){
+            return [$output];
+        }
+        return $output;
+    }
+    
+    /**
+     * Returns testresults provided by the engine
+    */
+    public function getResults()
+    {
+        $variables = [];
+        return $variables;
+    }
+    
+    public function jsonSerialize()
+    {
+        return [
+        ];
+    }
+}
